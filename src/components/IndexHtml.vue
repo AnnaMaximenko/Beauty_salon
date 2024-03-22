140c31af662a3dd14624a81f5f79946eacb801e9
AnyaMaximenko <any-maksimenk@yandex.ru>
2024-03-22 14:36:33 +0300

    workin on bugs 1.2

--- src/components/IndexHtml.vue
+++ src/components/IndexHtml.vue
@@ -6,27 +6,6 @@
         Студия реконструкции <br />и&nbsp;колористики<br />&laquo;Best
         hair&raquo;
       </h2>
-      <!-- <div class="introduction__wrapper">
-        <svg
-          class="introduction__svg"
-          width="20"
-          height="30"
-          viewBox="0 0 20 30"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect
-            x="0.5"
-            y="0.5"
-            width="19"
-            height="29"
-            rx="7.5"
-            stroke="white"
-          />
-          <circle cx="10" cy="9" r="2" fill="white" />
-        </svg>
-        <p class="introduction__text">Прокрутите вниз</p>
-      </div> -->
     </div>
   </section>
   <section class="information center">
